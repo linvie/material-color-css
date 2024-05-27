ad2536f3c683aac5094d5b22ee5bb4825fead4c6
linvie <Li_Ching_@outlook.com>
2024-05-27 16:15:09 +0800

    update example

--- docs/example.js
+++ docs/example.js
@@ -5,19 +5,17 @@ import {
 
 const button = document.querySelector("button");
 button.addEventListener("click", () => {
-  console.log("clicked");
   updateColors("#ffffff");
 });
 
 const color = document.querySelector('input[type="color"]');
 color.addEventListener("input", (e) => {
   const color = e.target.value;
-  console.log(color);
+  // console.log(color);
   updateColors(color);
 });
 
 //color from image
-
 const fileInput = document.querySelector('input[type="file"]');
 fileInput.addEventListener("change", (event) => {
   const file = event.target.files[0];
